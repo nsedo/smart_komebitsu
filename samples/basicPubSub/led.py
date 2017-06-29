aa899bbdf69dddc6967c01d47ca597acac981050
nsedo <sedorick@gmail.com>
2017-06-29 16:40:10 +0900

    コメント追加

--- samples/basicPubSub/led.py
+++ samples/basicPubSub/led.py
@@ -11,6 +11,7 @@ GPIO.setup(33,GPIO.OUT)
 GPIO.setup(35,GPIO.OUT)
 GPIO.setup(37,GPIO.OUT)
 
+#LED点滅
 def flashing(pin,fcnt):
 	count = 0;
 	while count < fcnt:
@@ -20,12 +21,15 @@ def flashing(pin,fcnt):
 		time.sleep(0.5)
 		count += 1
 
+#LED点灯
 def light_on(pin):
         GPIO.output(pin,True)
 
+#LED消灯
 def light_off(pin):
         GPIO.output(pin,False)
 
+#赤LED点灯
 def light_on_red(num):
         pin = 11
         if num == 1 :
@@ -36,6 +40,7 @@ def light_on_red(num):
                 pin = 15
         light_on(pin)
 
+#緑LED点灯
 def light_on_green(num):
         pin = 33
         if num == 1 :
@@ -46,16 +51,19 @@ def light_on_green(num):
                 pin = 37
         light_on(pin)
 
+#赤LED全消灯
 def light_off_all_red():
         GPIO.output(11,False)
         GPIO.output(13,False)
         GPIO.output(15,False)
 
+#緑LED全消灯
 def light_off_all_green():
         GPIO.output(33,False)
         GPIO.output(35,False)
         GPIO.output(37,False)
 
+#LED全消灯
 def all_off():
         GPIO.output(11,False)
         GPIO.output(13,False)
